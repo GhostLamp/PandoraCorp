cd7afaebd253fa9a025061e76d167a153334bcd0
Riollito <smallghostlamp@gmail.com>
2025-11-01 22:02:56 -0300

    removing the test music
    dont want to blast people with airship fort

--- audio_manager.gd
+++ audio_manager.gd
@@ -5,10 +5,12 @@ var current_area:String
 var current_Low_time:String
 
 func _ready() -> void:
+	return
 	current_area = AudioGlobal.current_area
 	current_Low_time = AudioGlobal.low_time_left
 
 func _process(_delta: float) -> void:
+	return
 	if current_area != AudioGlobal.current_area or current_Low_time != AudioGlobal.low_time_left:
 		current_area = AudioGlobal.current_area
 		current_Low_time = AudioGlobal.low_time_left
@@ -16,4 +18,5 @@ func _process(_delta: float) -> void:
 		background_music_player.play()
 
 func update_music():
+	return
 	background_music_player["parameters/switch_to_clip"] = str(current_area + "Music" + current_Low_time)
